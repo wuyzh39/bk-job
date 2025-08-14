78f4a64b3066844732b39a0474032bb18aff12ae
lorryyzwu <lorryyzwu@tencent.com>
2025-08-14 21:28:38 +0800

    feat: 提供使用标准HTTP协议的ESB接口
    新增v4接口的全局异常处理器，新增v4_get_job_instance_list接口

--- src/backend/commons/common/src/main/java/com/tencent/bk/job/common/constant/ErrorCode.java
+++ src/backend/commons/common/src/main/java/com/tencent/bk/job/common/constant/ErrorCode.java
@@ -29,6 +29,13 @@ package com.tencent.bk.job.common.constant;
  * <p>
  * 错误码分类： 文件磁盘系统 59 MySQL系统 52 Redis系统 50 MQ 55 GSE 10 CMDB 11 作业平台 12 PAAS 13
  * </p>
+ *
+ * @since 2025-08-14
+ * 在蓝鲸新版 HTTP API 协议中：
+ *   错误码不应用于返回展示给用户，响应使用标准 HTTP 状态码
+ *   错误码应仅用于系统内部错误处理，可用于平台开发排查问题
+ *   错误码可用于上下文传递，获取对应描述
+ *
  */
 public class ErrorCode {
 
